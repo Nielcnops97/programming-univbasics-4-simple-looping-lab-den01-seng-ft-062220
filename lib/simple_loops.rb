8e21d29101ce0e366f01724397da1dae4b6ff9d7
Niel Cnops <nielcnops@outlook.com>
2020-06-10 20:03:10 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,24 @@
 # Write your methods here
+def loop_message_five_times(array)
+  count = 0
+  while count < 5
+  puts array 
+  count += 1
+ end
+end
+
+def loop_message_n_times(message, number)
+  count = 0
+  while count < number 
+  puts message 
+  count += 1
+ end
+end
+
+def output_array(message, number)
+  count = 0
+  while count < number
+  puts message
+  count += 1
+ end
+end
